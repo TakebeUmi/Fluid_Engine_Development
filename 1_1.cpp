fcb898d871fcb7d1f3e5aa40f945f3a3342bc3db
takebe <taketommar@yahoo.co.jp>
2024-12-24 16:58:36 +0900

    totyuu

--- 1_1.cpp
+++ 1_1.cpp
@@ -1,5 +1,39 @@
 #include <cstdio>
+// #include <array>
+const size_t kBufferSize = 80;
+using namespace std;
+void updateWave(const double timeInterval,double* x, double* speed) {
+    (*x) += timeInterval * (*speed);
+    if ((*x) > 1.0) {
+        (*speed) *= -1.0;
+        (*x) = 1.0 + timeInterval * (*speed);
+    }
+    else if ((*x) < 0.0) {
+        (*speed) *= -1.0;
+        (*x) = timeInterval * (*speed);
+    }
+}
+
+#include <cmath>
+
+void accumulateWaveToHeightField(
+    const double x,
+    const double waveLength,
+    const double maxHeight,
+    array<double, kBufferSize>* heightField
+) {
+    const double quarterWaveLength = 0.25 * waveLength;
+    const int start = static_cast<int>((x - quarterWaveLength) * kBufferSize);
+    const int end = static_cast<int>(())
+}
+
 int main() {
+    const double waveLengthX = 0.8;
+    const double waveLengthY = 1.2;
+
+    const double maxHeightX = 0.5;
+    const double maxHeightY = 0.4;
+
     double x = 0.0;
     double y = 1.0;
     double speedX = 1.0;
@@ -8,12 +42,13 @@ int main() {
     const int fps = 100;
     const double timeInterval = 1.0 / fps;
 
-    for (int i = 0; i < 1000;++i) {
+    array<double, kBufferSize> heightField;
 
+    for (int i = 0; i < 1000;++i) {
+        updateWave(timeInterval, &x, &speedX);
+        updateWave(timeInterval, &y, &speedY);
     }
-    void updateWave(const double timeInterval, double* x, double* speed) {
-        (*x) += timeInterval * (*speed);
-    }
+
 
     return 0;
 }
